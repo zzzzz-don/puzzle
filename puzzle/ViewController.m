368f6331507e69abd05a63aa69981034ecf3be11
Hidehiko Ishii <zhide0620@yahoo.co.jp>
2015-10-12 12:09:34 +0900

    画像を1/3にトリミング

--- puzzle/ViewController.m
+++ puzzle/ViewController.m
@@ -18,13 +18,26 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    // オリジナル画像を読み込み
     UIImage *imageData = [UIImage imageNamed:@"inu.png"];
     
+    // オリジナル画像をCGImageRef型に変換
+    CGImageRef imageDataRef = [imageData CGImage];
+    
+    // GCImageRef型のデータを利用してトリミング
+    CGImageRef trimmedImageDataRef =
+    CGImageCreateWithImageInRect(imageDataRef, CGRectMake(0, 0, 200, 100));
+    
+    // トリミングしたデータをUIImageクラスに戻す
+    UIImage *trimmedImageData = [UIImage imageWithCGImage:trimmedImageDataRef];
+    
+    // イメージビューの用意
     UIImageView *iv = [[UIImageView alloc] init];
-    iv.frame = CGRectMake(10, 20, 200, 300);
+    iv.frame = CGRectMake(10, 20, 200, 100);
     [self.view addSubview:iv];
     
-    iv.image = imageData;
+    // イメージビューに画像を貼り付ける
+    iv.image = trimmedImageData;
 }
 
 - (void)didReceiveMemoryWarning {
