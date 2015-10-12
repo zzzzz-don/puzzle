991d6a546bcdda07d300fa8badb0bb6a78bb243b
Hidehiko Ishii <zhide0620@yahoo.co.jp>
2015-10-12 11:31:40 +0900

    Initial Commit

---
+++ puzzle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  puzzle
+//
+//  Created by 石井 秀彦 on 2015/10/12.
+//  Copyright (c) 2015年 don. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
